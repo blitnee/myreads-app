8cd9e2f676118d483fb9a6002f77f06f633c0f42
blitnee <brittneelc@gmail.com>
2018-04-06 11:15:09 -0400

    feat: Import defined components
    ListBooks, SearchBooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Route } from 'react-router-dom'
+import ListBooks from './ListBooks'
+import SearchBooks from './SearchBooks'
 //import * as BooksAPI from './BooksAPI'
 import './App.css'
 
