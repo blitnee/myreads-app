5102018258668a6ff1f92765bbcb01195d5a40c8
blitnee <brittneelc@gmail.com>
2018-04-06 11:18:19 -0400

    feat: Add sortBy

--- src/SearchBooks.js
+++ src/SearchBooks.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
+import sortBy from 'sort-by'
 
 class SearchBooks extends Component {
 
