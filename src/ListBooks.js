bc6c025675c15645a1f64e491db536d614842ec6
blitnee <brittneelc@gmail.com>
2018-04-06 11:48:46 -0400

    feat: Link to search page
    Replace onClick with Link

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -164,7 +164,7 @@ class ListBooks extends Component {
           </div>
         </div>
         <div className="open-search">
-          <a onClick={() => this.setState({ showSearchPage: true })}>Add a book</a>
+          <Link to='/search'>Add a book</Link>
         </div>
       </div>
     )
