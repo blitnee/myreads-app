62bc00a2e47ed2ec2829dfe0d72c36a7a8fb6d7d
blitnee <brittneelc@gmail.com>
2018-04-06 10:54:08 -0400

    feat: Add ListBooks Route

--- src/App.js
+++ src/App.js
@@ -16,6 +16,9 @@ class BooksApp extends React.Component {
   render() {
     return (
       <div className="app">
+          <Route exact path='/' render={() => (
+            <ListBooks/>
+          )}/>
       </div>
     )
   }
