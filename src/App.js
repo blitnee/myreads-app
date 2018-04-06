2ddbb236c87f9a074ee8fd5b00a4188f07fdaf93
blitnee <brittneelc@gmail.com>
2018-04-06 11:42:50 -0400

    feat: Add placeholder methods
    Still determining features and functionality.

--- src/App.js
+++ src/App.js
@@ -2,18 +2,30 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import ListBooks from './ListBooks'
 import SearchBooks from './SearchBooks'
-//import * as BooksAPI from './BooksAPI'
+import * as BooksAPI from './BooksAPI'
 import './App.css'
 
 class App extends React.Component {
   state = {
+    books: []
     /**
      * TODO: Instead of using this state variable to keep track of which page
      * we're on, use the URL in the browser's address bar. This will ensure that
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    // showSearchPage: false
+  }
+
+  moveBook = (book) => {
+
+  }
+
+  deleteBook = (book) => {
+
+  }
+
+  addBook(book) {
   }
 
   render() {
@@ -21,10 +33,19 @@ class App extends React.Component {
     return (
       <div className="app">
           <Route exact path='/' render={() => (
-            <ListBooks/>
+            <ListBooks
+              books={this.state.books}
+              onMoveBook={this.moveBook}
+              onDeleteBook={this.deleteBook}
+            />
           )}/>
           <Route path='/search' render={({ history }) => (
-            <SearchBooks/>
+            <SearchBooks
+              onAddBook={(book) => {
+                this.addBook(book)
+                history.push('/')
+              }}
+            />
           )}/>
       </div>
     )
