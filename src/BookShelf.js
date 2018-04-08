e4412da73960cbfa2d80ee152ad95809bb975a72
blitnee <brittneelc@gmail.com>
2018-04-08 12:37:11 -0400

    feat: Add BookShelf Component
    Will hold BookLists

---
+++ src/BookShelf.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import BookList from './BookList'
+
+class BookShelf extends Component {
+
+  static PropTypes = {
+    title: PropTypes.string.isRequired,
+    id: PropTypes.string.isRequired,
+    // books: PropTypes.array.isRequired
+  }
+
+
+
+  render() {
+    const { title, id } = this.props
+
+    return (
+
+      <div key={ id } className="bookshelf">
+        <h2 className="bookshelf-title">{ title }</h2>
+        <div className="bookshelf-books">
+            <BookList />
+        </div>
+      </div>
+    )
+  }
+
+}
+
+export default BookShelf
