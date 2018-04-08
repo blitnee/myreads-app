8ae48be50d046c06828c0b0fd0ff7aebe59c113f
blitnee <brittneelc@gmail.com>
2018-04-08 15:01:35 -0400

    feat: Add changeShelf fx

--- src/BookShelf.js
+++ src/BookShelf.js
@@ -1,16 +1,27 @@
 import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import BookList from './BookList'
+import * as BooksAPI from './BooksAPI'
 
 class BookShelf extends Component {
 
   static PropTypes = {
     title: PropTypes.string.isRequired,
     id: PropTypes.string.isRequired,
-    // books: PropTypes.array.isRequired
   }
 
+  state = {
+    myBooks: []
+  }
 
+  changeShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf).then(book => {
+      this.setState(state => ({
+        myBooks: state.myBooks.filter((b) => b.shelf)
+      }))
+    })
+    // update view on change
+  }
 
   render() {
     const { title, id } = this.props
@@ -20,7 +31,11 @@ class BookShelf extends Component {
       <div key={ id } className="bookshelf">
         <h2 className="bookshelf-title">{ title }</h2>
         <div className="bookshelf-books">
-            <BookList id={id} />
+            <BookList id={id} onChangeShelf={(book, shelf) => {
+                this.changeShelf(book, shelf)
+                // history.push('/')
+              }}
+            />
         </div>
       </div>
     )
