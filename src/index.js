607fe9aac3f779386f7df7abacdba482589758ac
blitnee <brittneelc@gmail.com>
2018-04-17 17:00:37 -0400

    style: Start code on new line

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
 
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>,
+ReactDOM.render(
+	<BrowserRouter><App /></BrowserRouter>,
 	document.getElementById('root')
 );
