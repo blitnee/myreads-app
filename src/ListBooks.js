e12a2b2756df274e3073632f7c1c582c1a6eb931
blitnee <brittneelc@gmail.com>
2018-04-17 16:59:26 -0400

    feat: Delete BookShelf Component
    Map bookshelves instead of using a separate component.

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router-dom'
-import BookShelf from './BookShelf'
+import Books from './Books'
+import PropTypes from 'prop-types'
 
 class ListBooks extends Component {
 
+  static PropTypes = {
+    myBooks: PropTypes.array.isRequired
+  }
+
+    getBooks = (id) => {
+      return this.props.myBooks.filter(book => book.shelf === id)
+    }
+
   render() {
+
+    const bookshelves = [{
+        id: 'currentlyReading',
+        title: 'Currently Reading',
+        books: this.getBooks('currentlyReading')
+      },{
+        id: 'wantToRead',
+        title: 'Want To Read',
+        books: this.getBooks('wantToRead')
+      },{
+        id: 'read',
+        title: 'Read',
+        books: this.getBooks('read')
+      }]
 
     return (
 
@@ -12,13 +35,16 @@ class ListBooks extends Component {
         <div className="list-books-title">
           <h1>MyReads</h1>
         </div>
-
         <div className="list-books-content">
-          <BookShelf id='currentlyReading' title='Currently Reading' />
-          <BookShelf id='wantToRead' title='Want To Read' />
-          <BookShelf id='read' title='Read' />
+          {bookshelves.map( shelf =>
+            <div key={ shelf.id } className="bookshelf">
+              <h2 className="bookshelf-title">{ shelf.title }</h2>
+                <div className="bookshelf-books">
+                  <Books books={ shelf.books } />
+                </div>
+            </div>
+          )}
         </div>
-
         <div className="open-search">
           <Link to='/search'>Add Books</Link>
         </div>
