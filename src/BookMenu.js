20427c102937f6d6f3c6f2decc5b876ec1ef919d
blitnee <brittneelc@gmail.com>
2018-05-06 18:28:06 -0400

    style: change shelf ->new value

--- src/BookMenu.js
+++ src/BookMenu.js
@@ -4,28 +4,34 @@ import PropTypes from 'prop-types'
 class BookMenu extends Component {
 
   static PropTypes = {
-    onChangeShelf: PropTypes.func.isRequired,
+    onNewValue: PropTypes.func.isRequired,
     book: PropTypes.object.isRequired
   }
 
-/*
- * Book and e are submitting the proper info. Don't change this!
- */
   handleSubmit = (book , e) => {
     e.preventDefault();
-      this.props.onChangeShelf(book , e.target.value);
+    if(this.props.onNewValue){
       this.props.onNewValue(book , e.target.value);
     }
   }
 
+
+
   render() {
 
     const { book } = this.props
+    /*
+     * Should compare books in myBooks for nonSelect status or other value,
+     * or! Add book.shelf value to those already in myBooks from SearchBooks.js
+     */
+
+    // if (!book.shelf) {
+    //   book.shelf = 'noneSelect'
+    // }
 
   	return(
-
       <div className="book-shelf-changer">
-        <select value={ book.shelf } onChange={e => this.handleSubmit(book, e)}>
+        <select value={ book.shelf } onChange={ (e) => this.handleSubmit(book, e)}>
           <option value="none" disabled>Move to...</option>
           <option value="currentlyReading">Currently Reading</option>
           <option value="wantToRead">Want to Read</option>
@@ -33,7 +39,6 @@ class BookMenu extends Component {
           <option value="noneSelect">None</option>
         </select>
       </div>
-
 		)
 	}
 }
