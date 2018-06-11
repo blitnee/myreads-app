240709d08a2591c747c28b3ed4bfcfb44e5344ce
blitnee <brittneelc@gmail.com>
2018-06-11 12:15:36 -0400

    feat: Code cleanup for Author and Thumbnail

--- src/Book.js
+++ src/Book.js
@@ -37,17 +37,12 @@ class Book extends Component {
 				<div className="book">
 					<div className="book-top">
 						<div className="book-cover" style={{
-							width: 128, height: 193,
-							backgroundImage: `url(${book.imageLinks.thumbnail})`
+							width: 128, height: 193, backgroundImage: `url(${this.getThumbnail()})`
 							}}/>
-						<BookMenu
-							book={ book }
-							onNewValue={(book, shelf) => {
-								onChangeShelf(book, shelf) }}
-							/>
+						<BookMenu book={ book } />
 					</div>
 					<div className="book-title">{book.title}</div>
-					<div className="book-authors">{book.authors}</div>
+					<div className="book-authors">{this.getAuthors()}</div>
 				</div>
 			</li>
 		)
