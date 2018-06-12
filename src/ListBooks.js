6e4bc543d39251efdf362bd8f12b8fa7e560f442
blitnee <brittneelc@gmail.com>
2018-06-12 07:52:46 -0400

    refactor: Refactor bookshelves to object

--- src/ListBooks.js
+++ src/ListBooks.js
@@ -3,15 +3,26 @@ import { Link } from 'react-router-dom'
 import BookShelf from './BookShelf'
 
 function ListBooks () {
+	const bookShelves = [
+	{
+		shelfId: 'currentlyReading',
+		title: 'Currently Reading'
+	},{
+		shelfId: 'wantToRead',
+		title: 'Want To Read'
+	},{
+		shelfId: 'read',
+		title: 'Read'
+	}]
 	return (
 		<div className="list-books">
 			<div className="list-books-title">
 				<h1>MyReads</h1>
 			</div>
 			<div className="list-books-content">
-				<BookShelf shelfId="currentlyReading" title="Currently Reading" />
-				<BookShelf shelfId="wantToRead" title="Want To Read" />
-				<BookShelf shelfId="read" title="Read" />
+				{bookShelves.map(bookshelf => {
+					return <BookShelf shelfId={bookshelf.shelfId} title={bookshelf.title} />
+				})}
 			</div>
 			<div className="open-search">
 				<Link to='/search' className="add-button">Add Books</Link>
